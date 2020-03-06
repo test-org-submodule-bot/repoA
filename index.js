c95944f806b5ab01aa8fcd45da82d50f6374b235
Dave Balmain <dsbalmain@agiledigital.com.au>
2020-03-06 13:51:06 +1100

    Update repoC

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const repoB = require("./repoB");
-const repoC = require("./repoB");
+const repoC = require("./repoC");
 
 console.log(`repoB is at [${repoB.version()}]`);
 console.log(`repoC is at [${repoC.version()}]`);
